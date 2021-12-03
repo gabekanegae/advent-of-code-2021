20b27a86b9e0337fcd2a3de605e826eb91af6f45
Gabriel Kanegae <gabekanegae@gmail.com>
2021-12-03 03:11:57 -0300

    Day 3 - Modularize get_rating function

--- 03_binary_diagnostic.py
+++ 03_binary_diagnostic.py
@@ -19,46 +19,38 @@ def get_least_common_bit(report, pos, tiebreak=None):
     most = get_most_common_bit(report, pos)
     return {'0': '1', '1': '0', None: tiebreak}[most]
 
+def get_rating(report, keep_func, tiebreak):
+    pos = 0
+    l, r = 0, len(report)-1
+    while l < r:
+        keep = keep_func(report[l:r+1], pos, tiebreak=tiebreak)
+        if keep == '0':
+            while report[r][pos] == '1':
+                r -= 1
+        else:
+            while report[l][pos] == '0':
+                l += 1
+        pos += 1
+
+    return report[l]
+
 ####################################
 
 length = 12
 report = [str(r).zfill(length) for r in AOCUtils.load_input(3)]
 
-gamma = ''.join(get_most_common_bit(report, i) for i in range(length))
-epsilon = ''.join({'0': '1', '1': '0'}[i] for i in gamma)
+gamma_rate = ''.join(get_most_common_bit(report, i) for i in range(length))
+epsilon_rate = ''.join({'0': '1', '1': '0'}[i] for i in gamma_rate)
 
-power_consumption = int(gamma, 2) * int(epsilon, 2)
+power_consumption = int(gamma_rate, 2) * int(epsilon_rate, 2)
 print(f'Part 1: {power_consumption}')
 
 report.sort()
 
-pos = 0
-l, r = 0, len(report)-1
-while l < r:
-    most_common = get_most_common_bit(report[l:r+1], pos, tiebreak='1')
-    if most_common == '0':
-        while report[r][pos] == '1':
-            r -= 1
-    else:
-        while report[l][pos] == '0':
-            l += 1
-    pos += 1
-oxygen = report[l]
+oxygen_rating = get_rating(report, get_most_common_bit, tiebreak='1')
+co2_rating = get_rating(report, get_least_common_bit, tiebreak='0')
 
-pos = 0
-l, r = 0, len(report)-1
-while l < r:
-    least_common = get_least_common_bit(report[l:r+1], pos, tiebreak='0')
-    if least_common == '0':
-        while report[r][pos] == '1':
-            r -= 1
-    else:
-        while report[l][pos] == '0':
-            l += 1
-    pos += 1
-co2 = report[l]
-
-life_support = int(oxygen, 2) * int(co2, 2)
-print(f'Part 2: {life_support}')
+life_support_rating = int(oxygen_rating, 2) * int(co2_rating, 2)
+print(f'Part 2: {life_support_rating}')
 
 AOCUtils.print_time_taken()
