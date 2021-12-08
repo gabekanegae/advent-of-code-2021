2bf14badec6805d81840446f5a48be8789d53905
Gabriel Kanegae <gabekanegae@gmail.com>
2021-12-08 03:43:53 -0300

    Day 8 - Join both parts by decoding first

--- 08_seven_segment_search.py
+++ 08_seven_segment_search.py
@@ -4,25 +4,20 @@
 
 import AOCUtils
 
-easy_seg_amts = {
-    1: 2,
-    4: 4,
-    7: 3,
-    8: 7
-}
-
 def decode(entry_in, entry_out):
     mapping = dict()
 
-    # [1, 4, 7, 8]: can be determined instantly by their amount of segments (easy_seg_amts)
-    for n, seg_amt in easy_seg_amts.items():
+    # [1, 4, 7, 8]: can be determined instantly by their segment amount
+    easy_mapping = {1: 2, 4: 4, 7: 3, 8: 7}
+    for n, seg_amt in easy_mapping.items():
         mapping[n] = next(s for s in entry_in if len(s) == seg_amt)
 
+    # Group the ones left by segment amount
     possible_0_6_9 = set(s for s in entry_in if len(s) == 6)
     possible_2_3_5 = set(s for s in entry_in if len(s) == 5)
 
     # [0, 6, 9]: only 6 isn't a superset of 7
-    mapping[6] = next(s for s in possible_0_6_9 if not s >= mapping[7])
+    mapping[6] = next(s for s in possible_0_6_9 if not s > mapping[7])
     possible_0_9 = possible_0_6_9 - {mapping[6]}
 
     # [0, 9]: only 9 is a superset of 4 - 7
@@ -30,17 +25,16 @@ def decode(entry_in, entry_out):
     mapping[0] = (possible_0_9 - {mapping[9]}).pop()
 
     # [2, 3, 5]: only 5 is a subset of 6
-    mapping[5] = next(s for s in possible_2_3_5 if s <= mapping[6])
+    mapping[5] = next(s for s in possible_2_3_5 if s < mapping[6])
     possible_2_3 = possible_2_3_5 - {mapping[5]}
 
     # [2, 3]: only 3 is a subset of 9
-    mapping[3] = next(s for s in possible_2_3 if s <= mapping[9])
+    mapping[3] = next(s for s in possible_2_3 if s < mapping[9])
     mapping[2] = (possible_2_3 - {mapping[3]}).pop()
 
     reverse_mapping = {v: k for k, v in mapping.items()}
-    translated_out = [str(reverse_mapping[c]) for c in entry_out]
 
-    return int(''.join(translated_out))
+    return ''.join(str(reverse_mapping[c]) for c in entry_out)
 
 #######################################
 
@@ -55,13 +49,12 @@ for raw_entry in raw_entries:
 
     entries.append((entry_in, entry_out))
 
-count_1_4_7_8 = 0
-for entry_in, entry_out in entries:
-    count_1_4_7_8 += sum(len(c) in easy_seg_amts.values() for c in entry_out)
+decoded_entries = [decode(entry_in, entry_out) for entry_in, entry_out in entries]
 
-print(f'Part 1: {count_1_4_7_8}')
+p1 = sum(''.join(decoded_entries).count(i) for i in '1478')
+print(f'Part 1: {p1}')
 
-outputs_sum = sum(decode(entry_in, entry_out) for entry_in, entry_out in entries)
-print(f'Part 2: {outputs_sum}')
+p2 = sum(int(decoded_entry) for decoded_entry in decoded_entries)
+print(f'Part 2: {p2}')
 
 AOCUtils.print_time_taken()
