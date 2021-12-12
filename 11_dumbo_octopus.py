55066769dca6a1f5ac9b949000fb6b58e5897077
Gabriel Kanegae <gabekanegae@gmail.com>
2021-12-12 03:23:24 -0300

    Day 11 - Refactor to OOP

--- 11_dumbo_octopus.py
+++ 11_dumbo_octopus.py
@@ -10,34 +10,46 @@ mov8 = [
      (1, -1),  (1, 0),  (1, 1)
     ]
 
-def update(grid):
-    will_flash = set()
-    has_flashed = set()
+class OctopusGrid:
+    def __init__(self, grid):
+        self.grid = grid
 
-    for i in range(grid_size):
-        for j in range(grid_size):
-            grid[i][j] += 1
-            if grid[i][j] > 9:
-                will_flash.add((i, j))
+    def update(self):
+        will_flash = set()
+        has_flashed = set()
 
-    while will_flash:
-        flash_x, flash_y = will_flash.pop()
-        has_flashed.add((flash_x, flash_y))
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                self.grid[i][j] += 1
+                if self.grid[i][j] > 9:
+                    will_flash.add((i, j))
 
-        for dx, dy in mov8:
-            new_x, new_y = flash_x + dx, flash_y + dy
-            if not (0 <= new_x < grid_size and 0 <= new_y < grid_size):
-                continue
+        while will_flash:
+            flash_x, flash_y = will_flash.pop()
+            has_flashed.add((flash_x, flash_y))
 
-            if (new_x, new_y) in has_flashed:
-                continue
+            for dx, dy in mov8:
+                new_x, new_y = flash_x + dx, flash_y + dy
+                if not (0 <= new_x < len(grid) and 0 <= new_y < len(grid)):
+                    continue
 
-            grid[new_x][new_y] += 1
-            if grid[new_x][new_y] > 9:
-                will_flash.add((new_x, new_y))
+                if (new_x, new_y) in has_flashed:
+                    continue
 
-    for (i, j) in has_flashed:
-        grid[i][j] = 0
+                grid[new_x][new_y] += 1
+                if grid[new_x][new_y] > 9:
+                    will_flash.add((new_x, new_y))
+
+        for (i, j) in has_flashed:
+            self.grid[i][j] = 0
+
+    @property
+    def current_flashes(self):
+        return sum(row.count(0) for row in self.grid)
+
+    @property
+    def all_flashing(self):
+        return self.current_flashes == len(grid) * len(grid)
 
 #################################
 
@@ -46,23 +58,21 @@ grid_size = 10
 # My utils func read all lines as ints, so had to revert that
 grid = [list(map(int, str(r).zfill(grid_size))) for r in AOCUtils.load_input(11)]
 
+octopus_grid = OctopusGrid(grid)
+step = 0
+
 total_flashes = 0
 for _ in range(100):
-    update(grid)
+    octopus_grid.update()
+    step += 1
 
-    flashes = sum(row.count(0) for row in grid)
-    total_flashes += flashes
+    total_flashes += octopus_grid.current_flashes
 
 print(f'Part 1: {total_flashes}')
 
-step = 100
-while True:
+while not octopus_grid.all_flashing:
+    octopus_grid.update()
     step += 1
-    update(grid)
-
-    flashes = sum(row.count(0) for row in grid)
-    if flashes == grid_size * grid_size:
-        break
 
 print(f'Part 2: {step}')
 
