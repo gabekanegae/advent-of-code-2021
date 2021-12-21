d1b0a48c423c85c890e83f2f97eea983c2e8d62b
Gabriel Kanegae <gabekanegae@gmail.com>
2021-12-21 01:48:51 -0300

    Day 19 - Readability tweaks and comments

--- 19_beacon_scanner.py
+++ 19_beacon_scanner.py
@@ -7,7 +7,7 @@ from collections import defaultdict, deque
 import AOCUtils
 
 # http://www.euclideanspace.com/maths/algebra/matrix/transforms/examples/index.htm
-rotation_matrices = [
+rotations = [
     (( 1, 0, 0), ( 0, 1, 0), ( 0, 0, 1)), # ( x,  y,  z)
     (( 1, 0, 0), ( 0, 0,-1), ( 0, 1, 0)), # ( x, -z,  y)
     (( 1, 0, 0), ( 0,-1, 0), ( 0, 0,-1)), # ( x, -y, -z)
@@ -34,20 +34,17 @@ rotation_matrices = [
     (( 0, 1, 0), ( 0, 0,-1), (-1, 0, 0)), # ( y, -z, -x)
 ]
 
-def apply_rotation(rot_matrix, pos):
-    return tuple(sum(a * b for a, b in zip(pos, row)) for row in rot_matrix)
+def rotate_vectors(rotation, p):
+    return tuple(sum(a * b for a, b in zip(p, row)) for row in rotation)
 
-def sum_vectors(a, b):
+def add_vectors(a, b):
     return tuple(pa + pb for pa, pb in zip(a, b))
 
 def sub_vectors(a, b):
     return tuple(pa - pb for pa, pb in zip(a, b))
 
-def abs_vectors(a, b):
+def distance_vectors(a, b):
     return tuple(abs(pa - pb) for pa, pb in zip(a, b))
-
-def multiply_matrices(a, b):
-    return tuple(tuple(sum(pa * pb for pa, pb in zip(r_a, c_b)) for c_b in b) for r_a in a)
 
 class Scanner:
     def __init__(self, scanner_id, raw_beacons):
@@ -68,37 +65,88 @@ class Scanner:
         if self.rot is None and self.pos is None:
             return self._absolute_beacons
         
+        # Apply any existing rotation and translation to all beacons
         if self.rot is not None:
-            self._absolute_beacons = set(apply_rotation(self.rot, p) for p in self._absolute_beacons)
+            self._absolute_beacons = set(rotate_vectors(self.rot, p) for p in self._absolute_beacons)
         if self.pos is not None:
-            self._absolute_beacons = set(sum_vectors(self.pos, p) for p in self._absolute_beacons)
+            self._absolute_beacons = set(add_vectors(self.pos, p) for p in self._absolute_beacons)
         
         return self._absolute_beacons
 
     @property
     def distances(self):
-        return {tuple(sorted(abs_vectors(a, b))): (a, b) for a, b in combinations(self.beacons, 2)}
+        # Compute all n*(n-1)/2 distances between beacons,
+        # together with the beacon coordinates for lookup
+        return {tuple(sorted(distance_vectors(a, b))): (a, b) for a, b in combinations(self.beacons, 2)}
 
 def find_transformation(cur, ref):
+    # From the list of common distances, pick any arbitrary line segment
     common_distances = cur.distances.keys() & ref.distances.keys()
-    for distance in common_distances:
-        cur_pair = cur.distances[distance]
-        ref_pair = ref.distances[distance]
+    distance = common_distances.pop()
 
-        for rot in rotation_matrices:
-            rotated_cur_pair = tuple(apply_rotation(rot, p) for p in cur_pair)
+    # Get the two pairs of points related to that line segment, one from the
+    # reference beacon and another from the current, to-be-matched, beacon
+    cur_pair = cur.distances[distance]
+    ref_pair = ref.distances[distance]
 
-            translation_1 = [sub_vectors(b, a) for a, b in zip(rotated_cur_pair, ref_pair)]
-            translation_2 = [sub_vectors(b, a) for a, b in zip(reversed(rotated_cur_pair), ref_pair)]
+    # For each of the 24 rotations, rotate the pair from the current beacon
+    # and solve for a translation that would take the current line segment
+    # to the reference line segment
+    for rot in rotations:
+        rotated_cur_pair = tuple(rotate_vectors(rot, p) for p in cur_pair)
 
-            if translation_1[0] == translation_1[1]:
-                pos = translation_1[0]
-                return pos, rot
-            elif translation_2[0] == translation_2[1]:
-                pos = translation_2[0]
-                return pos, rot
+        # Attempt the two possible translations after this rotation,
+        # if none of them are valid, keep trying the other rotations
+        
+        # {cur[0]: ref[0], cur[1]: ref[1]}
+        translation = [sub_vectors(b, a) for a, b in zip(rotated_cur_pair, ref_pair)]
+        if translation[0] == translation[1]:
+            pos = translation[0]
+            return pos, rot
+
+        # {cur[0]: ref[1], cur[1]: ref[0]}
+        translation = [sub_vectors(b, a) for a, b in zip(rotated_cur_pair, reversed(ref_pair))]
+        if translation[0] == translation[1]:
+            pos = translation[0]
+            return pos, rot
 
     return None, None
+
+def reconstruct_map(scanners):
+    # Create graph of connected scanners
+    connections = defaultdict(set)
+    for a, b in combinations(scanners, 2):
+        # Scanners are connected if they have at least 12 beacons in common,
+        # i.e. C(12,2) = 66 beacon-to-beacon distances
+        if len(a.distances.keys() & b.distances.keys()) >= 66:
+            connections[a.scanner_id].add(b.scanner_id)
+            connections[b.scanner_id].add(a.scanner_id)
+
+    # Set scanner #0 as global origin
+    scanners[0].rot = rotations[0]
+    scanners[0].pos = (0, 0, 0)
+
+    # BFS through all scanners, rotating and translating their
+    # coordinates to match scanner #0 as global origin
+    queue = deque([(0, None)])
+    known_scanners = set()
+    while queue: 
+        cur_id, ref_id = queue.popleft()
+
+        if cur_id in known_scanners: continue
+        known_scanners.add(cur_id)
+
+        for connection_id in connections[cur_id]:
+            # Store parent scanner as reference
+            queue.append((connection_id, cur_id))
+
+        # Ignore transformations for scanner #0 as it is set as global origin
+        if cur_id == 0: continue
+
+        # Compute and apply transformation to current scanner   
+        cur = scanners[cur_id]
+        ref = scanners[ref_id]
+        cur.pos, cur.rot = find_transformation(cur, ref)
 
 ##################################
 
@@ -113,42 +161,12 @@ for scanner_id, raw_scanner in enumerate(raw_scanners):
     scanner = Scanner(scanner_id, raw_scanner)
     scanners.append(scanner)
 
-connections = defaultdict(set)
-for a, b in combinations(scanners, 2):
-    if len(a.distances.keys() & b.distances.keys()) >= 66: # C(12,2) = 12!/(2!*10!) = 66
-        connections[a.scanner_id].add(b.scanner_id)
-        connections[b.scanner_id].add(a.scanner_id)
+reconstruct_map(scanners)
 
-scanners[0].rot = rotation_matrices[0]
-scanners[0].pos = (0, 0, 0)
+unique_beacons = set().union(*(scanner.beacons for scanner in scanners))
+print(f'Part 1: {len(unique_beacons)}')
 
-queue = deque([(0, None)])
-known_scanners = set()
-while queue:
-    cur_id, ref_id = queue.popleft()
-
-    if cur_id in known_scanners: continue
-    known_scanners.add(cur_id)
-
-    for connection_id in connections[cur_id]:
-        queue.append((connection_id, cur_id))
-
-    if cur_id == 0: continue
-
-    cur = scanners[cur_id]
-    ref = scanners[ref_id]
-
-    cur.pos, cur.rot = find_transformation(cur, ref)
-
-    intersec = cur.beacons & ref.beacons
-
-beacons = set()
-for scanner in scanners:
-    beacons |= scanner.beacons
-
-print(f'Part 1: {len(beacons)}')
-
-max_distance = max(sum(abs_vectors(a.pos, b.pos)) for a, b in combinations(scanners, 2))
+max_distance = max(sum(distance_vectors(a.pos, b.pos)) for a, b in combinations(scanners, 2))
 print(f'Part 2: {max_distance}')
 
 AOCUtils.print_time_taken()
