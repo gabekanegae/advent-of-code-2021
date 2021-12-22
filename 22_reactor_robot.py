4324d05f93f22260de00b6c5b1b5ee0fcc0b2af3
Gabriel Kanegae <gabekanegae@gmail.com>
2021-12-22 13:12:10 -0300

    Day 22 - Flatten coordinate list

--- 22_reactor_robot.py
+++ 22_reactor_robot.py
@@ -15,19 +15,19 @@ def intersection(a, b):
 
     intersect = x_intersect and y_intersect and z_intersect
     if intersect:
-        x_min, x_max = max(a.x_min, b.x_min), min(a.x_max, b.x_max)
-        y_min, y_max = max(a.y_min, b.y_min), min(a.y_max, b.y_max)
-        z_min, z_max = max(a.z_min, b.z_min), min(a.z_max, b.z_max)
+        x_range = max(a.x_min, b.x_min), min(a.x_max, b.x_max)
+        y_range = max(a.y_min, b.y_min), min(a.y_max, b.y_max)
+        z_range = max(a.z_min, b.z_min), min(a.z_max, b.z_max)
         
-        return Cuboid(x_min, x_max, y_min, y_max, z_min, z_max)
+        return Cuboid(*x_range, *y_range, *z_range)
 
     return None
 
 def count_cubes_on(steps):
     # Keep dict of {cuboid: count} per the inclusion–exclusion principle
     cuboids = defaultdict(int)
-    for state, (x_min, x_max), (y_min, y_max), (z_min, z_max) in steps:
-        new_region = Cuboid(x_min, x_max, y_min, y_max, z_min, z_max)
+    for state, *bounds in steps:
+        new_region = Cuboid(*bounds)
 
         for existing_region in list(cuboids.keys()):
             intersecting_region = intersection(new_region, existing_region)
@@ -56,14 +56,15 @@ raw_steps = AOCUtils.load_input(22)
 
 steps = []
 for raw_step in raw_steps:
-    raw_step = raw_step.split()
+    step, raw_ranges = raw_step.split()
 
-    coords = [list(map(int, s[2:].split('..'))) for s in raw_step[1].split(',')]
-    step = tuple([raw_step[0]] + coords)
+    x_range, y_range, z_range = [map(int, s[2:].split('..')) for s in raw_ranges.split(',')]
 
+    step = tuple([step, *x_range, *y_range, *z_range])
     steps.append(step)
 
-init_steps = [step for step in steps if all(all(abs(c) <= 50 for c in p) for p in step[1:])]
+init_steps = [step for step in steps if all(abs(c) <= 50 for c in step[1:])]
+
 print(f'Part 1: {count_cubes_on(init_steps)}')
 
 print(f'Part 2: {count_cubes_on(steps)}')
