00cb6c9cae6e47c4543ec5750e5c1f6d9d62a97d
Gabriel Kanegae <gabekanegae@uber.com>
2021-12-25 05:02:20 -0300

    Day 25 - Refactor to OOP

--- 25_sea_cucumber.py
+++ 25_sea_cucumber.py
@@ -4,52 +4,73 @@
 
 import AOCUtils
 
-moves = {'>': lambda x, y: (x, (y+1)%size_y),
-         'v': lambda x, y: ((x+1)%size_x, y)}
+class SeaCucumberMap:
+    def __init__(self, raw_sea_cucumber_map):
+        self.moves = {
+            '>': lambda x, y: (x, (y+1)%self.size_y),
+            'v': lambda x, y: ((x+1)%self.size_x, y)
+        }
+
+        self.size_x = len(raw_sea_cucumber_map)
+        self.size_y = len(raw_sea_cucumber_map[0])
+
+        self.sea_cucumbers = dict()
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                if raw_sea_cucumber_map[x][y] != '.':
+                    self.sea_cucumbers[(x, y)] = raw_sea_cucumber_map[x][y]
+
+    def move(self):
+        moved = False
+
+        for sea_cucumber, nxt_fn in self.moves.items():
+            update = dict()
+            remove = set()
+
+            for cur in self.sea_cucumbers:
+                nxt = nxt_fn(*cur)
+
+                if self.sea_cucumbers[cur] == sea_cucumber:
+                    if nxt not in self.sea_cucumbers:
+                        remove.add(cur)
+                        update[nxt] = sea_cucumber
+
+            self.sea_cucumbers.update(update)
+            for i in remove: self.sea_cucumbers.pop(i)
+
+            moved |= bool(update) or bool(remove)
+
+        return moved
+
+    def __repr__(self):
+        s = []
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                cur = (x, y)
+
+                if cur in self.sea_cucumbers:
+                    c = self.sea_cucumbers[cur]
+                else:
+                    c = '.'
+
+                s.append(c)
+            s.append('\n')
+        
+        return ''.join(s)
 
 ################################
 
-raw_sea_cucumbers = AOCUtils.load_input(25)
+raw_sea_cucumber_map = AOCUtils.load_input(25)
 
-size_x = len(raw_sea_cucumbers)
-size_y = len(raw_sea_cucumbers[0])
-
-sea_cucumbers = dict()
-for x in range(size_x):
-    for y in range(size_y):
-        if raw_sea_cucumbers[x][y] != '.':
-            sea_cucumbers[(x, y)] = raw_sea_cucumbers[x][y]
+sea_cucumber_map = SeaCucumberMap(raw_sea_cucumber_map)
 
 steps = 0
 while True:
     steps += 1
-    changed = False
 
-    for cucumber, nxt_fn in moves.items():
-        update = dict()
-        remove = set()
+    # print(sea_cucumber_map)
 
-        for cur in sea_cucumbers:
-            nxt = nxt_fn(*cur)
-
-            if sea_cucumbers[cur] == cucumber:
-                if nxt not in sea_cucumbers:
-                    remove.add(cur)
-                    update[nxt] = cucumber
-
-        sea_cucumbers.update(update)
-        for i in remove: sea_cucumbers.pop(i)
-
-        changed |= bool(update) or bool(remove)
-
-    # for x in range(size_x):
-    #     for y in range(size_y):
-    #         cur = (x, y)
-    #         print(end=(sea_cucumbers[cur] if cur in sea_cucumbers else '.'))
-    #     print()
-    # input()
-
-    if not changed:
+    if not sea_cucumber_map.move():
         break
 
 print(f'Part 1: {steps}')
